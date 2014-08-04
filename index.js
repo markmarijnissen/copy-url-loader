e265c15e6314d21c60acf2f4eca1028b66a01cc6
Mark Marijnissen <markmarijnissen@gmail.com>
2014-08-04 11:30:28 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
+
+	Modified to use copy-loader by Mark Marijnissen
+*/
+var loaderUtils = require("loader-utils");
+var mime = require("mime");
+module.exports = function(content) {
+	this.cacheable && this.cacheable();
+	var query = loaderUtils.parseQuery(this.query);
+	var limit = (this.options && this.options.url && this.options.url.dataUrlLimit) || 0;
+	if(query.limit) {
+		limit = parseInt(query.limit, 10);
+	}
+	var mimetype = query.mimetype || query.minetype || mime.lookup(this.resourcePath);
+	if(limit <= 0 || content.length < limit) {
+		return "module.exports = " + JSON.stringify("data:" + (mimetype ? mimetype + ";" : "") + "base64," + content.toString("base64"));
+	} else {
+		var copyLoader = require("copy-loader");
+		return copyLoader.call(this, content);
+	}
+}
+module.exports.raw = true;
